16c112362adbf46b33991c5438ec072ba61067b7
Friska <72170157@students.ukdw.ac.id>
2019-09-03 18:10:49 +0900

    Commit tugas 2

---
+++ app/src/main/java/ukdw/com/proteintracker/HelpActivity.java
@@ -0,0 +1,22 @@
+package ukdw.com.proteintracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        TextView myTextView = new TextView(this);
+        Bundle b = getIntent().getExtras();
+
+        String helpText = b.getString("helpString");
+        myTextView.setText("Some Help Text");
+        setContentView(myTextView);
+    }
+}
